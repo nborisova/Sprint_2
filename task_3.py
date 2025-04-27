e85b55e1e187244fa62ee8d6c6631f059e82d2fe
Natalia Borisova <borisova.natalia.14@gmail.com>
2025-04-27 21:50:00 +0200

    fix for task 3

--- task_3.py
+++ task_3.py
@@ -1,8 +1,8 @@
-points = 0
-
 class PointsForPlace:
     @staticmethod
     def get_points_for_place(place):
+        points = 0
+
         if place > 100:
             print('Баллы начисляются только первым 100 участникам')
         elif place < 1:
@@ -14,6 +14,8 @@ class PointsForPlace:
 class PointsForMeters:
     @staticmethod
     def get_points_for_meters(meters):
+        points = 0
+
         if meters < 0:
             print('Количество метров не может быть отрицательным')
         else:
@@ -21,9 +23,8 @@ class PointsForMeters:
             return points     
 
 class TotalPoints(PointsForPlace, PointsForMeters):
-    @staticmethod
-    def get_total_points(meters, place):
-        total = PointsForPlace.get_points_for_place(place) + PointsForMeters.get_points_for_meters(meters)
+    def get_total_points(self, meters, place):
+        total = super().get_points_for_place(place) + PointsForMeters.get_points_for_meters(meters)
         return total
 
 points_for_place = PointsForPlace()
